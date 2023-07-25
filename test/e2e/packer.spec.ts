e72aeb2cd0ef5f2b4da8b732a3fc25e3759031e3
goodandco <oleksandr.hudenko@gmail.com>
2023-07-25 10:37:07 +0200

    added test cases

--- test/e2e/packer.spec.ts
+++ test/e2e/packer.spec.ts
@@ -1,15 +1,24 @@
 import * as assert from 'assert';
-import Packer from '../../src/packer';
+import APIException from '../../src/errors/api-exception';
 // @ts-ignore
 import * as fixtures from './fixtures/fixtures.json';
+import Packer from '../../src/packer';
+
 describe('Packer Tests', () => {
   // arrange
-  for (const { name, filePath, expected } of fixtures) {
+  for (const { name, filePath, expected, isThrowingError = false } of fixtures) {
     it(name, () => {
-      // act
-      const actual = Packer.pack(__dirname + '/fixtures/' + filePath);
-      // assert
-      assert.equal(actual, expected);
+      const absoluteFilePath = __dirname + '/fixtures/' + filePath;
+      const test = () => Packer.pack(absoluteFilePath);
+      if (isThrowingError) {
+        // act + assert
+        assert.throws(test, APIException)
+      } else {
+        // act
+        const actual = test();
+        // assert
+        assert.equal(actual, expected);
+      }
     });
   }
 
