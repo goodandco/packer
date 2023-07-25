d800928073ced9285a794d5da4cff7c6dd3abd65
goodandco <oleksandr.hudenko@gmail.com>
2023-07-26 00:11:03 +0200

    Added comments and little refactoring of calculator and map-reduce algorithm

--- src/algorithm/map-reduce.ts
+++ src/algorithm/map-reduce.ts
@@ -1,10 +1,31 @@
 import { TCombinationVariant, TInputDataRowItem, TSumVariant } from '../types';
 import { IRowCalculateAlgorithm } from '../interfaces';
+import { MAX_ITEM_COST, MAX_ITEM_WEIGHT } from '../constants';
+import { APIException } from '../errors';
 
 export class MapReduce implements IRowCalculateAlgorithm {
   run(maxWeight: number, items: Array<TInputDataRowItem>): Array<number> {
+    // filtering all the items with weight less than max weight for package
+    // and validating MAX_ITEM_WEIGHT & MAX_ITEM_COST
+    const filteredItems = items.filter(([, itemWeight, itemCost]) => {
+      if (itemWeight > MAX_ITEM_WEIGHT) {
+        throw new APIException(
+          `Max weight for the item should be not more than ${MAX_ITEM_WEIGHT}. Your value is: ${itemWeight}.`,
+        );
+      }
+      if (itemCost > MAX_ITEM_COST) {
+        throw new APIException(
+          `Max cost for the item should be not more than ${MAX_ITEM_COST}. Your value is: ${itemCost}.`,
+        );
+      }
+
+      return itemWeight <= maxWeight;
+    });
+
+    if (!filteredItems.length) return [];
+
     // necessary to asc sorting to be sure that next element has bigger weight
-    const sorted = items.sort(([, aWeight], [, bWeight]) => {
+    const sorted = filteredItems.sort(([, aWeight], [, bWeight]) => {
       if (aWeight < bWeight) return -1;
       if (aWeight > bWeight) return 1;
       return 0;
@@ -14,6 +35,10 @@ export class MapReduce implements IRowCalculateAlgorithm {
     const result = sorted
       // prepare list of all possible combinations: Array<TCombinationVariant>
       .reduce((acc, [id, weight, price]) => {
+        // if there is very begining of calculation then we add item to the first subset
+        // because in previous step we filtered the data and there is no weight more then maxWeight
+        // also if there is no way to add even item with minimal weight to curernt items' weight
+        // and be in range of maxWeight, then we also create subset from only one current item
         if (acc.length === 0 || weight + minItemWeight > maxWeight) {
           acc.push({
             items: [{ id, weight, price }],
@@ -21,20 +46,27 @@ export class MapReduce implements IRowCalculateAlgorithm {
           });
         } else {
           const accLen = acc.length;
+          // replacement method
           for (let j = 0; j < accLen; j++) {
             const { items, sum } = acc[j] as TCombinationVariant;
+            // diff shows the an amount of weight which could be added after we add
+            // current weight to subarray's weight sum
             const diff = sum + weight - maxWeight;
+            // if it less then zero it means we are able to add new subset
+            // which included acc[j] subset + new one
             if (diff <= 0) {
               acc.push({
                 items: [...items, { id, weight, price }],
                 sum: sum + weight,
               });
             } else {
-              const foundToReplace = items.find(
+              // here we're going to check if we have some elements which we
+              // could replace with our current item and be in maxWeight range
+              const foundToReplaceList = items.filter(
                 (item) => diff - item.weight <= 0,
               );
 
-              if (foundToReplace) {
+              for (const foundToReplace of foundToReplaceList) {
                 acc.push({
                   items: [
                     ...items.filter((item) => item.id !== foundToReplace.id),
--- src/client/default-pack-calculator.ts
+++ src/client/default-pack-calculator.ts
@@ -1,10 +1,5 @@
 import { TInputData, TInputDataRow, TPackerResult } from '../types';
-import {
-  MAX_ITEM_WEIGHT,
-  MAX_ITEM_COST,
-  MAX_ITEMS_LENGTH,
-  MAX_PACK_WEIGHT,
-} from '../constants';
+import { MAX_ITEMS_LENGTH, MAX_PACK_WEIGHT } from '../constants';
 import { IPackCalculator, IRowCalculateAlgorithm } from '../interfaces';
 import { APIException } from '../errors';
 
@@ -26,26 +21,8 @@ export class DefaultPackCalculator implements IPackCalculator {
           `Max items for a pack should be not more than ${MAX_ITEMS_LENGTH}. Your value is: ${items.length}.`,
         );
       }
-      // filtering all the items with weight less then max weight for package
-      // and validating MAX_ITEM_WEIGHT
-      const preparedItems = items.filter(([, itemWeight, itemCost]) => {
-        if (itemWeight > MAX_ITEM_WEIGHT) {
-          throw new APIException(
-            `Max weight for the item should be not more than ${MAX_ITEM_WEIGHT}. Your value is: ${itemWeight}.`,
-          );
-        }
-        if (itemCost > MAX_ITEM_COST) {
-          throw new APIException(
-            `Max cost for the item should be not more than ${MAX_ITEM_COST}. Your value is: ${itemCost}.`,
-          );
-        }
 
-        return itemWeight <= maxWeight;
-      });
-
-      if (!preparedItems.length) return [];
-
-      return this.rowCalculateAlgorithm.run(maxWeight, preparedItems);
+      return this.rowCalculateAlgorithm.run(maxWeight, items);
     });
   }
 }
